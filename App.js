ba24482300194f41b8a0a331bb5680bccc3c516d
betomoedano <betomoedano@outlook.com>
2021-12-09 16:57:59 -0500

    add firebase auth

--- App.js
+++ App.js
@@ -1,47 +1,113 @@
-import React from 'react';
-import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button } from 'react-native';
+import React, {useEffect} from 'react';
+import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
 import { BlurView } from 'expo-blur';
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './firebase-config';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+
 
 const uri = 'https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg'
 const profilePicture = 'https://randomuser.me/api/portraits/men/34.jpg'
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
-      <View style={{width: 100, height: 100, backgroundColor: 'purple', position: 'absolute' }}></View>
-      <View style={{width: 100, height: 100, backgroundColor: 'blue', top: 120, position: 'absolute', transform: [{rotate: '25deg'}] }}></View>
-      <View style={{width: 100, height: 100, backgroundColor: 'red', bottom: 120 ,position: 'absolute', borderRadius: 50, transform: [{rotate: '50deg'}] }}></View>
-      <ScrollView contentContainerStyle= {{
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}> 
-        <BlurView intensity={100}>
-          <View style={styles.login}>
-            <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
-            <View>
-              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>E-mail</Text>
-              <TextInput style={styles.input} placeholder="betomoedano@outlook.com" />
-            </View>
-            <View>
-              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Password</Text>
-              <TextInput style={styles.input} placeholder="password" secureTextEntry={true}/>
-            </View>
-            <TouchableOpacity style={[styles.button, {backgroundColor: '#00CFEB90'}]}>
-              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Login</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.button, {backgroundColor: '#6792F090'}]}>
-              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Create Account</Text>
-            </TouchableOpacity>
-          </View>
-        </BlurView>
-      </ScrollView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
+
+  function LoginScreen() {
+
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const navigation = useNavigation();
+
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+    const handleCreateAccount = () => {
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Account created!')
+        const user = userCredential.user;
+        console.log(user)
+      })
+      .catch(error => {
+        console.log(error)
+        Alert.alert(error.message)
+      })
+    }
+
+    const handleSignIn = () => {
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Signed in!')
+        const user = userCredential.user;
+        console.log(user)
+        navigation.navigate('Home');
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+
+
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
+        <View style={{width: 100, height: 100, backgroundColor: 'purple', position: 'absolute' }}></View>
+        <View style={{width: 100, height: 100, backgroundColor: 'blue', top: 120, position: 'absolute', transform: [{rotate: '25deg'}] }}></View>
+        <View style={{width: 100, height: 100, backgroundColor: 'red', bottom: 120 ,position: 'absolute', borderRadius: 50, transform: [{rotate: '50deg'}] }}></View>
+        <ScrollView contentContainerStyle= {{
+          flex: 1,
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}> 
+          <BlurView intensity={100}>
+            <View style={styles.login}>
+              <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
+              <View>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>E-mail</Text>
+                <TextInput onChangeText={(text) => setEmail(text)} style={styles.input} placeholder="betomoedano@outlook.com" />
+              </View>
+              <View>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Password</Text>
+                <TextInput onChangeText={(text) => setPassword(text)} style={styles.input} placeholder="password" secureTextEntry={true}/>
+              </View>
+              <TouchableOpacity onPress={handleSignIn} style={[styles.button, {backgroundColor: '#00CFEB90'}]}>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Login</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleCreateAccount} style={[styles.button, {backgroundColor: '#6792F090'}]}>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Create Account</Text>
+              </TouchableOpacity>
+            </View>
+          </BlurView>
+        </ScrollView>
+      </View>
+    );
+  }
+
+  const Stack = createNativeStackNavigator();
+  
+  export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCKMsuU_Ycs4RKF0adb3GQYRtK8WKtNBEo",
+  authDomain: "blur-login.firebaseapp.com",
+  projectId: "blur-login",
+  storageBucket: "blur-login.appspot.com",
+  messagingSenderId: "23088781334",
+  appId: "1:23088781334:web:e21d8186a42ec23dde7d37"
+};
